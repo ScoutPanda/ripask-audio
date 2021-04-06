bc9128ac7ae9f9bbd8c2eed2f85fbd4fb10487b2
ScoutPanda <eetu94@live.fi>
2021-04-06 14:22:33 +0300

    Service worker added
    Added service worker that currently only caches index.html, no-image.png and cover art images. Has the caveat that if the request fails, it will still be cached since Opaque requests do not contain the information if the request failed or succeeded...

---
+++ src/sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'ripask-cache-v1';
+const urlsToCache = [
+  'index.html',
+  'assets/no-image.png'
+];
+
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+
+self.addEventListener('activate', event => {
+  const currentCaches = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', function fetcher (event) {
+  if (event.request.url.indexOf('getCoverArt') > -1) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.match(event.request).then(function (response) {
+          return response || fetch(event.request).then(function(response) {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  }
+});
